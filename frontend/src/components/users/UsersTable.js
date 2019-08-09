7c5b869fce6999d2ef898c11b8950e5041b3dace
Oscar Olivares <oscarolivares625@gmail.com>
2019-08-09 16:53:46 -0400

    Se termina usersTable, vista que lista los usuarios

--- frontend/src/components/users/UsersTable.js
+++ frontend/src/components/users/UsersTable.js
@@ -3,43 +3,17 @@ import { Table } from 'react-materialize';
 
 import UsersEdit from './UsersEdit';
 import UsersDelete from './UsersDelete';
+import './UsersTable.css';
 
 const usersAPI = 'http://192.168.0.114:3000/api/v1/users';
 
-// Get users from server
-function getUsersList() {
-  const init = {
-    method: 'GET',
-    mode: 'cors'
-  };
-
-  const request = new Request(usersAPI, init);
-
-  fetch(request)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        console.log('on else');
-      }
-    })
-    .then(data => {
-      console.log(data[0]);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
 /* Rows for user table */
 function UsersTableRows(props) {
-  getUsersList();
-
   return (
     <tr>
-      <td>554ed928f33c4</td>
-      <td>Alvin</td>
-      <td>Eclair</td>
+      <td className="hidden-sm">{props.user._id}</td>
+      <td>{props.user.firstname}</td>
+      <td>{props.user.lastname}</td>
 
       {/* Actions */}
       <td>
@@ -50,25 +24,66 @@ function UsersTableRows(props) {
   );
 }
 
-/* List of users */
-function UsersTable() {
-  return (
-    <Table striped centered>
-      <thead>
-        <tr>
-          <th data-field="_id">ID</th>
-          <th data-field="firstname">First Name</th>
-          <th data-field="lastname">Last Name</th>
-          <th data-field="actions">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <UsersTableRows />
-        {/* <UsersTableRows />
-        <UsersTableRows /> */}
-      </tbody>
-    </Table>
-  );
+// User table
+class UsersTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(usersAPI, { method: 'GET', mode: 'cors' })
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            users: result
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, users } = this.state;
+    const rows = [];
+
+    if (error) {
+      return <div>Conexion problem. Please try again later</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      rows.push(
+        users.map(user => <UsersTableRows user={user} key={user._id} />)
+      );
+
+      return (
+        <Table striped centered>
+          <thead>
+            <tr>
+              <th data-field="_id" className="hidden-sm">
+                ID
+              </th>
+              <th data-field="firstname">First Name</th>
+              <th data-field="lastname">Last Name</th>
+              <th data-field="actions">Actions</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      );
+    }
+  }
 }
 
 export default UsersTable;
